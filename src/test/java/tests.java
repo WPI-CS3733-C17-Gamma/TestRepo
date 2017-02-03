2f238bfb96e17372d5334aae7b37b961475bfb18
Adam Goldsmith <adam@adamgoldsmith.name>
2017-02-03 13:46:15 -0500

    Failing Test

---
+++ src/test/java/tests.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class tests {
+  @Test
+  public void testTrueTrue() {
+	  assertEquals(true, true);
+	  assertTrue(true);
+	  assertFalse(true);
+  }
+}
